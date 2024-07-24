ef7e1026efb7a4992ed274ca632359d5ba8f8492
Anees <31478058+ShadowWalker91@users.noreply.github.com>
2024-07-24 11:11:26 +0500

    X (Twitter) API based BOT
    It performs 18 twitter operations like auto follow, auto like, auto retweet

---
+++ secret.py
@@ -0,0 +1,14 @@
+#ANEES APP KEYS
+
+ammar_new = {
+    'CONSUMER_KEY':'c4ug8dKgJcRExxM6niTYylwEz',
+    'CONSUMER_SECRET':'dWCfkAjB3Eja51d5JT1jH6qF779EQUzA72exOak6lAO8uXD3tp',
+    'ACCESS_TOKEN':'1504065387180310529-9moosbl2IFoZ0myWBPup4IVCp4jmPg',
+    'ACCESS_SECRET':'qlp6RGsvqRciJ4kzVbUGy2M54c3tpCtJrEUJwFa7xYrRx',
+    'FILE_NAME':'test_user_data.txt',
+    'LOG_FILE':'test_log_file.txt'
+}
+
+
+
+accounts = [ammar_new]
