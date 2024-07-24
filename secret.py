26671f547120d484f3841c5e0c83929473189e90
Anees <31478058+ShadowWalker91@users.noreply.github.com>
2024-07-24 11:14:11 +0500

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,14 +1,14 @@
 #ANEES APP KEYS
 
-ammar_new = {
-    'CONSUMER_KEY':'c4ug8dKgJcRExxM6niTYylwEz',
-    'CONSUMER_SECRET':'dWCfkAjB3Eja51d5JT1jH6qF779EQUzA72exOak6lAO8uXD3tp',
-    'ACCESS_TOKEN':'1504065387180310529-9moosbl2IFoZ0myWBPup4IVCp4jmPg',
-    'ACCESS_SECRET':'qlp6RGsvqRciJ4kzVbUGy2M54c3tpCtJrEUJwFa7xYrRx',
+Xid01 = {
+    'CONSUMER_KEY':'your_consumer_key',
+    'CONSUMER_SECRET':'your_consumer_secret',
+    'ACCESS_TOKEN':'your_access_token',
+    'ACCESS_SECRET':'your_access_secret',
     'FILE_NAME':'test_user_data.txt',
     'LOG_FILE':'test_log_file.txt'
 }
 
 
 
-accounts = [ammar_new]
+accounts = [Xid01]
